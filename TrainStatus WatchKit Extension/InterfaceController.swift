a6e8b464f5104ada1f712d02439ed6e655f01e8e
Brad Moore <brad.moore@bydeluxe.com>
2018-10-31 07:25:05 -0700

    Initial Commit

---
+++ TrainStatus WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TrainStatus WatchKit Extension
+//
+//  Created by Brad Moore on 10/31/18.
+//  Copyright © 2018 Brad Moore. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ TrainStatus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrainStatus
+//
+//  Created by Brad Moore on 10/31/18.
+//  Copyright © 2018 Brad Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
