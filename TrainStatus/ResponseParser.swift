fe34a33f084f885d0bf280acae4a433c2eb3a363
Brad Moore <brad.moore@bydeluxe.com>
2018-11-03 16:19:09 -0700

    Put response in a file and load it for the test

---
+++ TrainStatus/ResponseParser.swift
@@ -0,0 +1,15 @@
+//
+//  ResponseParser.swift
+//  TrainStatus
+//
+//  Created by Brad Moore on 11/3/18.
+//  Copyright © 2018 Brad Moore. All rights reserved.
+//
+
+import Foundation
+
+class ResponseParser {
+    static func parse(body: String) -> String {
+        return ""
+    }
+}
---
+++ TrainStatusTests/ResponseParserTests.swift
@@ -0,0 +1,34 @@
+//
+//  ResponseParserTests.swift
+//  TrainStatusTests
+//
+//  Created by Brad Moore on 11/3/18.
+//  Copyright © 2018 Brad Moore. All rights reserved.
+//
+
+import XCTest
+@testable import TrainStatus
+
+class ResponseParserTests: XCTestCase {
+    
+    var content: String = ""
+
+    override func setUp() {
+        let path = Bundle(for: ResponseParserTests.self).path(forResource: "response", ofType: "txt")
+        do {
+            content = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+        
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        XCTAssertEqual(ResponseParser.parse(body: content), "")
+    }
+
+}
